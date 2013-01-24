9ba33b9a6a42cd5535d32db8c760d3c6addeee63
Michael Trunner <michael@trunner.de>
2013-01-24 15:53:35 +0100

    Code clean up

--- src/mouse.py
+++ src/mouse.py
@@ -55,27 +55,27 @@ class WindowManager(object):
         desk_output = subprocess.check_output("wmctrl -d", shell=True)
         desk_output = desk_output.strip().split('\n')
         self.desk_list = [line.split()[0] for line in desk_output]
-    
+
         current = filter(lambda x: x.split()[1] == "*" , desk_output)[0].split()
-    
+
         self.desktop = int(current[0])
-        
+
         width = int(current[8].split("x")[0])
         height = int(current[8].split("x")[1])
         self.size = (width, height)
-        
+
         orig_x = int(current[7].split(",")[0])
         orig_y = int(current[7].split(",")[1])
         self.offset = (orig_x, orig_y)
-        self.border = (top+bottom, left+right)
-        
+        self.border = (top + bottom, left + right)
+
         logger.info("Desktop dimensions are: (%d, %d)  (%d, %d)" % (orig_x,
                                                                     orig_y,
                                                                     width,
                                                                     height))
-        self.grid = (map(lambda x: int(round(x*width)), xgrid),
-                     map(lambda y: int(round(y*height)), ygrid))
-        
+        self.grid = (map(lambda x: int(round(x * width)), xgrid),
+                     map(lambda y: int(round(y * height)), ygrid))
+
         logger.info("x-Grid is %s" % self.grid[0])
         logger.info("y-Grid is %s" % self.grid[1])
         self.metacity = metacity
@@ -87,71 +87,74 @@ class WindowManager(object):
         win_output = subprocess.check_output("wmctrl -lG", shell=True)
         win_output = win_output.strip().split("\n")
         win_list = {}
-    
+
         for desk in self.desk_list:
-            win_list[desk] = map(lambda y: hex(int(y.split()[0],16)),
+            win_list[desk] = map(lambda y: hex(int(y.split()[0], 16)),
                                 filter(lambda x: x.split()[1] == desk,
-                                       win_output ))
+                                       win_output))
         return win_list
 
 
     def move_window_to_area(self, area):
-        posx = max(filter(lambda x: x < area.x1, self.grid[0]))
-        posy = max(filter(lambda x: x < area.y1, self.grid[1]))
-        w = min(filter(lambda x: x > area.x2, self.grid[0])) - posx
-        h = min(filter(lambda x: x > area.y2, self.grid[1])) - posy
-        logger.info("Moving window to %d, %d, %d, %d" % (posx, posy, w, h))
-        self.move_window(":ACTIVE:", posx, posy, w, h)
-        
+        try:
+            posx = max(filter(lambda x: x < area.x1, self.grid[0]))
+            posy = max(filter(lambda x: x < area.y1, self.grid[1]))
+            w = min(filter(lambda x: x > area.x2, self.grid[0])) - posx
+            h = min(filter(lambda x: x > area.y2, self.grid[1])) - posy
+            logger.info("Moving window to %d, %d, %d, %d" % (posx, posy, w, h))
+            self.move_window(":ACTIVE:", posx, posy, w, h)
+        except Exception as e:
+            print e
 
-    def move_window(self, windowid,x,y,w,h):
+
+    def move_window(self, windowid, x, y, w, h):
         """
         Resizes and moves the given window to the given position and dimensions
         """
         # This function is from stiler (https://github.com/soulfx/stiler)
-        x = int(x)+self.offset[0]
-        y = int(y)+self.offset[1]
-        
+        x = int(x) + self.offset[0]
+        y = int(y) + self.offset[1]
+
         max_h, max_v = False, False
         if self.max:
             if w == self.size[0]:
                 max_h = True
             if h == self.size[1]:
                 max_v = True
-        
-        h = h-self.border[0]
-        w = w-self.border[1]
-        
+
+        h = h - self.border[0]
+        w = w - self.border[1]
+
         if max_h:
             w = -1
         if max_v:
             h = -1
-        
+
         logging.debug("moving window: %s to (%s,%s,%s,%s) " %
-                      (windowid,x,y,w,h))
-        
+                      (windowid, x, y, w, h))
+
         if windowid == ":ACTIVE:":
-            window = "-r "+windowid
+            window = "-r " + windowid
         else:
-            window = "-i -r "+windowid
-    
+            window = "-i -r " + windowid
+
         # unmaximize
-        cmd="wmctrl " + window + " -b remove,maximized_vert,maximized_horz"
+        cmd = "wmctrl " + window + " -b remove,maximized_vert,maximized_horz"
         self._call(cmd)
-        
+
         # NOTE: metacity doesn't like resizing and moving in the same step    
         if self.metacity:
             # resize
             cmd = "wmctrl %s -e 0,-1,-1,%i,%i" % (window, w, h)
             self._call(cmd)
             # move    
-            cmd = "wmctrl %s -e 0,%i,%i,-1,-1" % (window,max(x,0), max(y,0))
+            cmd = "wmctrl %s -e 0,%i,%i,-1,-1" % (window, max(x, 0), max(y, 0))
             self._call(cmd)
         else:
-            cmd = "wmctrl %s -e 0,%i,%i,%i,%i" % (window, 
-                                                  max(x,0), max(y,0),
+            cmd = "wmctrl %s -e 0,%i,%i,%i,%i" % (window,
+                                                  max(x, 0), max(y, 0),
                                                   w, h)
-        
+
 
         if max_h:
             cmd = "wmctrl %s -b add,maximized_horz" % window
@@ -159,14 +162,14 @@ class WindowManager(object):
         if max_v:
             cmd = "wmctrl %s -b add,maximized_vert" % window
             self._call(cmd)
-        
+
         # set properties
         command = "wmctrl " + window + " -b remove,hidden,shaded"
-        self._call(command)      
+        self._call(command)
 
 
     def get_mouse_on_desktop(self, x, y):
-        return (x-self.offset[0], y-self.offset[1])
+        return (x - self.offset[0], y - self.offset[1])
 
 
     def _call(self, cmd):
@@ -176,37 +179,36 @@ class WindowManager(object):
 
 
 class Area(object):
-    
+
     def __init__(self, x, y):
         self.x1 = x
         self.x2 = x
         self.y1 = y
         self.y2 = y
 
-   
     def add_point(self, x, y):
         self.x1 = min(x, self.x1)
         self.x2 = max(x , self.x2)
         self.y1 = min(y, self.y1)
-        self.y2 = max(y, self.y2)    
-
+        self.y2 = max(y, self.y2)
 
     def __str__(self):
         return "Area: (%d, %d - %d, %d)" % (self.x1, self.y1,
                                         self.x2, self.y2)
-
 
     def __repr__(self):
         return self.__str__()
 
 
 class GridMouseVooDoo(Thread):
-    
+
     def __init__(self, wm, button=10, daemon=True):
         Thread.__init__(self)
+        
+        # Set the Thread.daemon flag
         self.daemon = daemon
         self.button = button
-        
+
         self.display = Display()
         self.ctx = self.display.record_create_context(
             0,
@@ -222,7 +224,7 @@ class GridMouseVooDoo(Thread):
                     'client_started': False,
                     'client_died': False,
             }])
-        
+
         self.wm = wm
         self.area = None
 
@@ -230,7 +232,7 @@ class GridMouseVooDoo(Thread):
     def run(self):
         logger.info("Starting Thread")
         self.display.record_enable_context(self.ctx, self.handler)
-        self.display.record_free_context(self.ctx)        
+        self.display.record_free_context(self.ctx)
 
 
     def handler(self, reply):
@@ -247,41 +249,43 @@ class GridMouseVooDoo(Thread):
                     self.release(event.root_x, event.root_y)
             else:
                 self.move(event.root_x, event.root_y)
-            
-            
+
+
     def stop(self):
         logger.info("Stopping Thread")
         self.display.record_disable_context(self.ctx)
         self.display.ungrab_pointer(X.CurrentTime)
         self.display.flush()
         logger.debug("Thread stopped")
-        
-        
+
+
     def press(self, x, y):
-        self.area = Area(x,y)
+        self.area = Area(x, y)
         logger.info("Mouse button pressed")
         self.move(x, y)
- 
-    
+
+
     def release(self, x, y):
         self.move(x, y)
-        logger.info( "%s" % self.area )
+        logger.info("%s" % self.area)
         self.wm.move_window_to_area(self.area)
         self.area = None
         logger.info("Mouse button released")
- 
-    
-    def move(self, x,y):
+
+
+    def move(self, x, y):
         x, y = wm.get_mouse_on_desktop(x, y)
         if not self.area:
             logger.debug("Mouse moved to %d, %d but not button not pressed" %
-                         (x,y))
+                         (x, y))
             return
         self.area.add_point(x, y)
-        logger.debug("Mouse moved to %d, %d" % (x,y))
+        logger.debug("Mouse moved to %d, %d" % (x, y))
 
 
-wm = WindowManager(([0, 0.33, 0.5, 0.67, 1],[0, 0.5, 1]), (32,1,1,1))
-button = 13        
+import time
+time.sleep(15)
+wm = WindowManager(([0, 0.33, 0.5, 0.67, 1], [0, 0.5, 1]), (32, 1, 1, 1))
+button = 13
 gmvd = GridMouseVooDoo(wm, button, False)
 gmvd.start()
